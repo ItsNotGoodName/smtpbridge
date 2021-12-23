29b30cbfa8cdf0c927bf5e827dc15eb9edf686c6
ItsNotGoodName <gurnaindeol@gmail.com>
2021-12-22 19:09:39 -0800

    refactor: remove mock db content

--- right/database/mock.go
+++ right/database/mock.go
@@ -1,41 +1,20 @@
 package database
 
 import (
-	"sync"
-
 	"github.com/ItsNotGoodName/smtpbridge/app"
 )
 
 type Mock struct {
-	db     map[string]app.Message
-	dbLock sync.RWMutex
 }
 
 func NewMock() *Mock {
-	return &Mock{
-		db:     make(map[string]app.Message),
-		dbLock: sync.RWMutex{},
-	}
+	return &Mock{}
 }
 
 func (db *Mock) Create(msg *app.Message) error {
-	db.dbLock.Lock()
-	db.db[msg.UUID] = *msg
-	db.dbLock.Unlock()
-
 	return nil
 }
 
 func (db *Mock) Update(msg *app.Message) error {
-	db.dbLock.Lock()
-	defer db.dbLock.Unlock()
-
-	_, ok := db.db[msg.UUID]
-	if !ok {
-		return app.ErrMessageNotFound
-	}
-
-	db.db[msg.UUID] = *msg
-
 	return nil
 }
