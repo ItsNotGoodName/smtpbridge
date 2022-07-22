13866f957a54327c77421742a1458cff3bc3a3d9
ItsNotGoodName <gurnaindeol@gmail.com>
2022-07-21 22:14:56 -0700

    refactor: change helper links

--- left/view/helper.go
+++ left/view/helper.go
@@ -16,16 +16,19 @@ var helperMap template.FuncMap = template.FuncMap{
 	"pageLink": func(page paginate.Page, newPage int) string {
 		return fmt.Sprintf("?page=%d&ascending=%v&limit=%d", newPage, page.Ascending, page.Limit)
 	},
-	"pageToggleAscendingLink": func(page paginate.Page) string {
+	"pageLinkToggleAscending": func(page paginate.Page) string {
 		return fmt.Sprintf("?page=%d&ascending=%v&limit=%d", page.Page, !page.Ascending, page.Limit)
 	},
-	"pageLimitLink": func(page paginate.Page, limit int) string {
+	"pageLinkLimit": func(page paginate.Page, limit int) string {
 		return fmt.Sprintf("?page=%d&ascending=%v&limit=%d", ((page.Page-1)*page.Limit)/limit+1, page.Ascending, limit)
 	},
-	"attachmentDataLink": func(att *envelope.Attachment) string {
+	"dataLinkAttachment": func(att *envelope.Attachment) string {
 		return fmt.Sprintf("/data/%s", att.FileName())
 	},
-	"envelopeIDLink": func(id int64) string {
+	"envelopeLink": func(env *envelope.Envelope) string {
+		return fmt.Sprintf("/envelopes/%d", env.Message.ID)
+	},
+	"envelopeLinkID": func(id int64) string {
 		return fmt.Sprintf("/envelopes/%d", id)
 	},
 }
