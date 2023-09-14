15f686cfb742e74e8ed59047d6d79d208cfe2a6f
ItsNotGoodName <gurnaindeol@gmail.com>
2023-09-14 01:01:23 -0700

    fix(database): hopefully fix database lock

--- internal/database/database.go
+++ internal/database/database.go
@@ -23,8 +23,8 @@ type QuerierTx interface {
 }
 
 func New(dbPath string, debug bool) (Querier, error) {
-	// https://github.com/pocketbase/pocketbase/blob/94a1cc07d5ed26bb9ba277506448b4fa14ef4bd9/core/db_nocgo.go#L14
-	pragmas := "?_pragma=busy_timeout(10000)&_pragma=journal_mode(WAL)&_pragma=journal_size_limit(200000000)&_pragma=synchronous(NORMAL)&_pragma=foreign_keys(ON)"
+	// https://github.com/pocketbase/pocketbase/blob/2608efb56cd4dab3cccfea5d6822cb87f7308636/core/db_nocgo.go#L14
+	pragmas := "?_pragma=busy_timeout(10000)&_pragma=journal_mode(WAL)&_pragma=journal_size_limit(200000000)&_pragma=synchronous(NORMAL)&_pragma=foreign_keys(ON)&_pragma=temp_store(MEMORY)&_pragma=cache_size(-16000)"
 	db, err := sql.Open("sqlite", dbPath+pragmas)
 	if err != nil {
 		return nil, err
