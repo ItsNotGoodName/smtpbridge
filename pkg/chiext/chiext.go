8310194827782cce1b0ad96af87406fb2f2dde79
ItsNotGoodName <gurnaindeol@gmail.com>
2023-09-19 18:08:16 -0700

    feat(pkg): add cache control middleware

--- pkg/chiext/chiext.go
+++ pkg/chiext/chiext.go
@@ -1,6 +1,7 @@
 package chiext
 
 import (
+	"fmt"
 	"io/fs"
 	"net/http"
 
@@ -22,5 +23,20 @@ func MountFS(r chi.Router, f fs.FS) {
 		}
 	} else if err != fs.ErrNotExist {
 		panic(err)
+	}
+}
+
+func CacheControl(maxAge int) func(h http.Handler) http.Handler {
+	var value string
+	if maxAge == 0 {
+		value = "max-age=31536000,immutable"
+	} else {
+		value = fmt.Sprintf("max-age=%d", maxAge)
+	}
+	return func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Cache-Control", value)
+			h.ServeHTTP(w, r)
+		})
 	}
 }
